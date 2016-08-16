57318dbe886437fcd2f8f37cbfec19582aa62572
mgoofyy <mgoofyy@gmail.com>
2016-08-16 20:37:52 +0800

    change the type

--- redis.js
+++ redis.js
@@ -20,18 +20,41 @@ exports.throw = function(bottle,callback) {
 };
 
 exports.picks = function(info,callback) {
+	
 	client.SELECT('all',function(){
 		//
 		//一定几率捞取到海星
 		//
-		client.RANDOMKEY(function(err,bottleId){
-			if (!bottleId) {
-				return callback({'code':'0','msg':'什么都没有哦','method':'post'});
-			}
-			client.HGETALL(bottleId,function(err,bottle){
-				callback({'code':'1','msg':'获取到瓶子','method':'POST','data':bottle});
-			});
+		//
+		// var type = {all:info.type}
+		if (Math.random() <= 0.2) {
+			return callback({'code':'0','msg':'获取了海星','method':'POST'});
+		}
+
+		var type = {all:Math.round(Math.random()), male:0 ,female:1};
+
+		info.type = info.type || 'all';
+
+		console.log(info);
+		
+		client.SELECT(type[info.type],function(){
+			 
+			client.RANDOMKEY(function(err,bottleId){
+
+				if (!bottleId) {
+					return callback({'code':'0','msg':'什么都没有哦','method':'post'});
+				}
+				client.HGETALL(bottleId,function(err,bottle){
+					if (err) {
+						return callback({'code':'0','msg':'漂流瓶子破损了','method':'post'});
+					}
+					// client.DEL(bottleId);
+					callback({'code':'1','msg':'获取到瓶子','method':'POST','data':bottle});
+				});
 			
+			});
 		});
+
+		
 	});
 };
